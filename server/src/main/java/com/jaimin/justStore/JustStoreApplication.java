72941d52a24fb3ff8f4d34ef0ae1a91ce3e2074d
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2025-12-29 22:47:18 +0530

    Init spring server

---
+++ server/src/main/java/com/jaimin/justStore/JustStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jaimin.justStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustStoreApplication.class, args);
+	}
+
+}
