c0adfd70a2181f5ea3080710e58e669534bd5211
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2026-01-02 11:25:29 +0530

    update logic to retrieve video

--- server/src/main/java/com/jaimin/justStore/utils/RetrieveVideo.java
+++ server/src/main/java/com/jaimin/justStore/utils/RetrieveVideo.java
@@ -1,75 +1,74 @@
 package com.jaimin.justStore.utils;
-import org.jcodec.api.FrameGrab;
-import org.jcodec.api.JCodecException;
-import org.jcodec.common.io.NIOUtils;
-import org.jcodec.common.model.Picture;
-import org.jcodec.scale.AWTUtil;
 
-import java.awt.image.BufferedImage;
+import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.*;
+
 import java.io.*;
-import java.nio.file.Files;
 import java.util.Arrays;
-
 
 public class RetrieveVideo {
 
+    public static byte[] decodeVideo(InputStream inputStream) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-    public static byte[] decodeVideo(String inputVideoPath) throws IOException, JCodecException {
-        File tempFile = File.createTempFile("retrieve_video_", ".tmp");
-        tempFile.deleteOnExit();
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputStream);
+        grabber.start();
 
-        try {
-            File file = new File(inputVideoPath);
-            FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(file));
-
-            try (FileOutputStream fos = new FileOutputStream(tempFile, false);
-                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-
-                Picture picture = grab.getNativeFrame();
-                int totalBytes = getMetadataFromFrame(picture);
-                while (null != (picture = grab.getNativeFrame())) {
-                    frameToByteArray(picture, bos, totalBytes);
-                }
-                bos.flush();
+        try (BufferedOutputStream bos = new BufferedOutputStream(baos)) {
+            Frame frame = grabber.grabImage();
+            if (frame == null) {
+                throw new IOException("No frames found in video");
             }
 
-            return Files.readAllBytes(tempFile.toPath());
+            int totalBytes = getMetadataFromFrame(frame);
+
+            while ((frame = grabber.grabImage()) != null) {
+                frameToByteArray(frame, bos, totalBytes);
+            }
+            bos.flush();
         } finally {
-            tempFile.delete();
+            grabber.stop();
+            grabber.release();
         }
+
+        return baos.toByteArray();
     }
 
-    static int getMetadataFromFrame(Picture picture) {
-        BufferedImage frame = AWTUtil.toBufferedImage(picture);
+    static int getMetadataFromFrame(Frame frame) {
+        Mat mat = new OpenCVFrameConverter.ToMat().convert(frame);
+
         int totalBytes = 0;
         for (int k = 0; k < 32; k++) {
-            int rgb = frame.getRGB(k, 0);
-            int red = (rgb >> 16) & 0xFF;
-            int green = (rgb >> 8) & 0xFF;
-            int blue = rgb & 0xFF;
+            int[] rgb = getPixelRGB(mat, k, 0);
+            int red = rgb[0];
+            int green = rgb[1];
+            int blue = rgb[2];
+
             boolean isWhite = (red > 128) && (green > 128) && (blue > 128);
             if (isWhite) {
-                totalBytes |= (1 << (k));
+                totalBytes |= (1 << k);
             }
         }
         System.out.println("Total length : " + totalBytes);
         return totalBytes;
     }
 
-    static void frameToByteArray(Picture picture, BufferedOutputStream bos, int totalBytes) throws IOException {
-        BufferedImage frame = AWTUtil.toBufferedImage(picture);
-        final int height = frame.getHeight();
-        final int width = frame.getWidth();
+    static void frameToByteArray(Frame frame, BufferedOutputStream bos, int totalBytes) throws IOException {
+        Mat mat = new OpenCVFrameConverter.ToMat().convert(frame);
+
+        final int height = mat.rows();
+        final int width = mat.cols();
         byte[] bytes = new byte[width / 8];
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j += 8) {
                 byte myByte = 0;
                 for (int k = 0; k < 8; k++) {
-                    int rgb = frame.getRGB(j + k, i);
-                    int red = (rgb >> 16) & 0xFF;
-                    int green = (rgb >> 8) & 0xFF;
-                    int blue = rgb & 0xFF;
+                    int[] rgb = getPixelRGB(mat, j + k, i);
+                    int red = rgb[0];
+                    int green = rgb[1];
+                    int blue = rgb[2];
+
                     boolean isWhite = (red > 128) && (green > 128) && (blue > 128);
                     if (isWhite) {
                         myByte |= (byte) (1 << (7 - k));
@@ -84,7 +83,17 @@ public class RetrieveVideo {
                 }
             }
             bos.write(bytes);
-
         }
+    }
+
+    private static int[] getPixelRGB(Mat mat, int x, int y) {
+        byte[] data = new byte[3];
+        mat.ptr(y, x).get(data);
+
+        int blue = data[0] & 0xFF;
+        int green = data[1] & 0xFF;
+        int red = data[2] & 0xFF;
+
+        return new int[]{red, green, blue};
     }
 }
